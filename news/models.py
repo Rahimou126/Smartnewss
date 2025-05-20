143946a73c04557ae2c83fcdf6e6254052877097
Rahimou126 <abdrahimselmi300@gmail.com>
2025-05-20 11:55:34 +0300

    fetch data feature

--- news/models.py
+++ news/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 
 
+
 # Create your models here.
 
 
--- news/views.py
+++ news/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+
+from django.shortcuts import render,reverse
+from django.template import context
+
+from .models import News
+from django.http import HttpResponse
+
+
 
 # Create your views here.
+
+
+
+def fetch(request):
+
+
+        news=News.objects.all().order_by('created_at')
+        context={'news':news}
+
+        return render(request,'news/listofnews.html',context)
+
+
+
+        
+
+    
+
+    
+       
+        
+
+
+    
+        
+        
+    
+
+
+
+    
+    
+    

37b1d27fbd8dd59aee6244b30496fea14f956c73
Rahimou126 <abdrahimselmi300@gmail.com>
2025-10-16 15:26:07 +0300

    correct last reviews

--- news/urls.py
+++ news/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
 
     path('fetch/',views.fetch,name='fetch'),
+    path('detail/',views.detail,name='detail'),
     
    
    
--- news/views.py
+++ news/views.py
@@ -3,12 +3,12 @@ import datetime
 from turtle import title
 
 from django.core.paginator import Paginator
-from django.shortcuts import render,reverse
-from django.template import context
+from django.shortcuts import render
+
 
 from .models import News
 from django.db.models import Q
-from django.http import Http404, HttpResponse
+from django.http import HttpResponse
 
 
 
@@ -30,18 +30,43 @@ def fetch(request):
         news = paginator.get_page(page)
         context={'news':news}
 
-        query=request.GET.get('query')
+        search=request.GET.get('search')
 
-        if query:
+        if search:
 
-                news=News.objects.filter(Q(Title__icontains=query) or Q(Content__icontains=query))
+                news=News.objects.filter(Q(Title__icontains=search) or Q(Content__icontains=search))
 
                 context={'news':news}
         else:
-                HttpResponse("No news found")
+                news=[ ]
+
+                
+        return render(request,'news/list.html',context)
 
 
-        return render(request,'news/listofnews.html',context)
+
+
+
+def detail(request):
+        if request.method=='GET':
+
+                news_detail_id=request.GET.get('id')
+                details=News.objects.get(id=news_detail_id)
+                context={'details':details}
+        
+        else:
+                HttpResponse("there is now details")
+        
+
+        return render(request,'news/detail.html',context)
+        
+        
+
+
+
+
+        
+
 
 
 
